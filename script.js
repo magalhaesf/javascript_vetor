bd54c9cfd17d92257a26b215b55dcba12d89b883
magalhaesf <fernanda.fatima.magalhaes@escola.pr.gov.br>
2025-04-24 08:51:15 -0300

    vetores

---
+++ script.js
@@ -0,0 +1,13 @@
+let numeros = [];
+
+numeros[0] = prompt();
+numeros[1] = prompt();
+numeros[2] = prompt();
+numeros[3] = prompt();
+numeros[4] = 90;
+
+for (let x =0; x < numeros.length; x++) {
+    console.log(numeros[x]);
+}
+
+console.log(numeros.length);
